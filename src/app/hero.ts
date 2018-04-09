729e973494ab79ca49a81f3ce588e1914a7e3029
Roger Saner <roger@burningdog.co.za>
2018-04-09 14:53:06 +0200

    feat: added a number of heroes, showing them in a styled list. Can click to edit

--- src/app/hero.ts
+++ src/app/hero.ts
@@ -1,4 +1,15 @@
 export class Hero {
-  id: number;
-  name: string;
+  constructor(id: number, name: string) {}
 }
+
+// Using the pattern from https://www.stevefenton.co.uk/2013/04/stop-manually-assigning-typescript-constructor-parameters/
+// Didn't use 'private' for the variables since I'm assigning the const HEROES to be of type Hero
+// which means being able to know that id and name are public properties
+
+// For simple models I prefer doing the following (i.e. models which don't require a constructor)
+// but used the above just to show a different example.
+
+// export class Hero {
+//   id: number;
+//   name: string;
+// }
